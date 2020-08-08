ebfec10f924c62947daf073c79426a1927a9187e
Mojgan360 <mojgan360@gmail.com>
2020-08-08 02:56:40 +0200

    Copmleted Profilo

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { makeStyles, withStyles } from '@material-ui/core/styles'
+import { TextField, Grid, Typography, Button, Box } from '@material-ui/core'
+
+//CSS
+const InputField = withStyles({
+  root: {
+    '& lable.mui-focused': {
+      color: 'tomato',
+    },
+  },
+})(TextField)
 
 const Contact = () => {
-  return <div>contact</div>
+  return (
+    <Box component='div'>
+      <Grid cintainer justify='center'>
+        <Box component='form'>
+          <Typography varient='h5' contact me></Typography>
+          <InputField fullWidth={true} lable='Name' variant='outlined' />
+        </Box>
+      </Grid>
+    </Box>
+  )
 }
 
 export default Contact
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,127 @@
 import React from 'react'
 
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import { Grid, Box } from '@material-ui/core'
+import img1 from '../../images/html-css-javascript.jpg'
+const useStyles = makeStyles({
+  mainContainer: {
+    background: 'var(--mainPrimary)',
+    height: '100%',
+  },
+  cardContainer: {
+    maxWidth: 345,
+    margin: '5rem',
+    margin: '3rem auto',
+  },
+})
 const Portfolio = () => {
-  return <div>Portfolio</div>
+  const classes = useStyles()
+  return (
+    <Box component='div' className={classes.mainContainer}>
+      <Grid container justify='center'>
+        {/* Project 1 */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardActionArea>
+              <CardMedia
+                component='img'
+                alt='Contemplative Reptile'
+                height='140'
+                image={img1}
+                title='Contemplative Reptile'
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='h2'>
+                  Lizard
+                </Typography>
+                <Typography variant='body2' color='textSecondary' component='p'>
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size='small' color='primary'>
+                Share
+              </Button>
+              <Button size='small' color='primary'>
+                Live Demo
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        {/* Project 2*/}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardActionArea>
+              <CardMedia
+                component='img'
+                alt='Contemplative Reptile'
+                height='140'
+                image={img1}
+                title='Contemplative Reptile'
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='h2'>
+                  Lizard
+                </Typography>
+                <Typography variant='body2' color='textSecondary' component='p'>
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size='small' color='primary'>
+                Share
+              </Button>
+              <Button size='small' color='primary'>
+                Live Demo
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        {/* Project 1 */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardActionArea>
+              <CardMedia
+                component='img'
+                alt='Contemplative Reptile'
+                height='140'
+                image={img1}
+                title='Contemplative Reptile'
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='h2'>
+                  Lizard
+                </Typography>
+                <Typography variant='body2' color='textSecondary' component='p'>
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size='small' color='primary'>
+                Share
+              </Button>
+              <Button size='small' color='primary'>
+                Live Demo
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  )
 }
 
 export default Portfolio
